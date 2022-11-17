0888e56b937622c0dc6f2d9bc0f6bc9af283b92c
rafi <raffizz.muhammed@gmail.com>
2022-11-18 00:40:09 +0530

    create a model and migrate

--- artGarage/artGarage/settings.py
+++ artGarage/artGarage/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -30,6 +32,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
+
+    
 ]
 
 MIDDLEWARE = [
--- artGarage/store/models.py
+++ artGarage/store/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class categories(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pic')
+    price =models.IntegerField()
+    date =models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name    
