dde01bb74acda932a87c64acaeb78a99e026014d
rafi <raffizz.muhammed@gmail.com>
2022-11-17 22:58:02 +0530

    project template added and change settings

--- artGarage/artGarage/settings.py
+++ artGarage/artGarage/settings.py
@@ -51,10 +51,11 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'artGarage.urls'
 
+import os
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
