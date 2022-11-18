4818bc6b74fddc345a2a659c44b97ebab2ee6339
rafi <raffizz.muhammed@gmail.com>
2022-11-18 06:48:56 +0530

    new model created and modified template

--- artGarage/store/admin.py
+++ artGarage/store/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import categories
+from .models import categories,collections
 
 # Register your models here.
 admin.site.register(categories)
+admin.site.register(collections)
--- artGarage/store/models.py
+++ artGarage/store/models.py
@@ -8,4 +8,15 @@ class categories(models.Model):
     price =models.IntegerField()
     date =models.DateTimeField(auto_now_add=True)
     def __str__(self):
-        return self.name    
+        return self.name 
+
+
+class collections(models.Model):   
+    elements = models.ForeignKey(categories,related_name ='element',on_delete =models.CASCADE)
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='pic')
+    desc = models.TextField()
+    price = models.IntegerField() 
+    date = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
