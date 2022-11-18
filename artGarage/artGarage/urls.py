d31331c282a21568cb41ec567ca9d4cdc37721a6
rafi <raffizz.muhammed@gmail.com>
2022-11-18 06:34:58 +0530

    added a new template,its collections

--- artGarage/artGarage/urls.py
+++ artGarage/artGarage/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.index),
+    path('',views.index,name = 'home'),
     path('category',views.category),
 ]
 urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
